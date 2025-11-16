406a887db243097bbeca251f845c852b19ad761d
Nageri Ibrahim <ibrahimdamilola43@gmail.com>
2025-11-16 18:56:49 +0100

    Spawn Commit

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -10,12 +10,18 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandom", 0.5f, Random.Range(0.3f, 2.0f));
     }
 
     // Update is called once per frame
-    void Update()
+    void SpawnRandom()
     {
-        
+        int ballIndex = Random.Range(0, ballPrefabs.Count);
+        int foodIndex = Random.Range(0, foodPrefabs.Count);
+        int crateIndex = Random.Range(0, cratePrefabs.Count);
+
+        Vector3 spawnPos = new Vector3(Random.Range(-4, 4), 13, 0);
+
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
     }
 }
