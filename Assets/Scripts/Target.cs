738ced5b293c4940f76a9019be690bc27c8ae87a
Nageri Ibrahim <ibrahimdamilola43@gmail.com>
2025-11-20 15:31:09 +0100

    Progress

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -11,27 +11,34 @@ public class Target : MonoBehaviour
     public ParticleSystem targetParticle;
     private GameManager gameManager;
 
+    public int count;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        rb.drag = 2;
     }
 
     private void OnMouseDown()
     {
-        while (gameManager.isGameActive)
-        {
             Destroy(gameObject);
             gameManager.score += 5;
             gameManager.scoreText.text = ":" + gameManager.score;
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        rb.drag -= Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        Debug.Log(count++);
 
     }
 }
